40c80c0dbace1e14026daeeaf049a3cbb58c04c7
Devendra-23 <dev6dhokare@gmail.com>
2023-08-03 00:03:04 +0100

    Removed Dummy Data and made data available on database(Firebase)

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,39 +1,41 @@
-import classes from "./AvailableMeals.module.css";
-import MealItem from "./MealItem/MealItem";
-import Card from "../UI/Card";
+// "https://food-order-6ad20-default-rtdb.europe-west1.firebasedatabase.app/meals.json";
+import { useEffect, useState } from "react";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Spicy Thai Green Curry",
-    description: "Fragrant, fiery, coconut-infused delight.",
-    price: 25.75,
-  },
-  {
-    id: "m2",
-    name: "Flavorful Chicken Shawarma",
-    description: "Juicy, spiced, tender meat.",
-    price: 30.0,
-  },
-  {
-    id: "m3",
-    name: "Delicious Chicken Tikka",
-    description: "Tandoori-spiced, grilled chicken skewers.",
-    price: 24.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy & green!",
-    price: 18.99,
-  },
-];
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://food-order-6ad20-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
-      id={meal.id}
       key={meal.id}
+      id={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}
